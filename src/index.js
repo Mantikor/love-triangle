b46acd2f8630f1917ae9683beebc351245e02f4a
Sergey Streltsov <sergey.streltsov@100nuts.ai>
2019-03-05 15:28:14 +0300

    eliminate var

--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    let triangles_count = Math.floor(preferences.filter((item, i) =>
+        preferences[preferences[preferences[i]-1]-1] === i+1).length/3);
+    return triangles_count;
 };
